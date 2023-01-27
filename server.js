f5b5747f23f5930184461febfea92f029e9ffabc
Rai_Froh <raifroh@gmail.com>
2023-01-26 18:26:14 -0800

    commit

--- server.js
+++ server.js
@@ -1,2 +1,8 @@
-const app = require('express')
+const express = require('express')
+const app = express()
 
+//serve up a file
+
+app.get('/', (request,response) => {
+    response.send('/index.html')
+})
