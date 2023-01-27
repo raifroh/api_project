1e0a82e970af9c9fd6be24f2df972d10bbec5fe3
Rai_Froh <raifroh@gmail.com>
2023-01-26 19:42:45 -0800

    last push of the day

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require('express')
 const app = express()
+let PORT = 8000
+
+const savage = {
+    'age': 29,
+    'birthName': '212121',
+    'birthLocation': 'London, England'
+}
+//create a server
+
+app.listen(PORT, () => {
+    console.log(`The server is now running on port ${PORT}!`)
+})
+
+//api
+app.get('/api', (request, response) => {
+    response.json(savage)
+})
 
 //serve up a file
 
 app.get('/', (request,response) => {
-    response.send('/index.html')
+    //this makes it check our current directory,
+    response.sendFile(__dirname + '/index.html')
 })
